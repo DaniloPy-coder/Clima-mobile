e35e51f4b4aefde6e376cdbe3d993583b19499f4
Danilo <danilodevsp@gmail.com>
2025-10-02 21:57:10 -0300

    chore: add css App

---
+++ App.styles.js
@@ -0,0 +1,47 @@
+import { StyleSheet } from "react-native";
+import { colors } from "./src/styles/colors";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.background,
+    },
+    scrollView: {
+        flexGrow: 1,
+    },
+    content: {
+        flexGrow: 1,
+        paddingHorizontal: 24,
+        paddingTop: 80,
+        paddingBottom: 24,
+    },
+    header: {
+        marginBottom: 24,
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        color: colors.text,
+        marginBottom: 8,
+    },
+    subtitle: {
+        fontSize: 16,
+        color: colors.textSecondary,
+    },
+    card: {
+        backgroundColor: colors.background,
+        borderRadius: 16,
+        padding: 24,
+        marginBottom: 24,
+    },
+    label: {
+        fontSize: 14,
+        color: colors.textSecondary,
+        marginBottom: 8,
+    },
+    currencyGrid: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+    }
+})
