83e90bc3ebd0fb5fe55569751725c28601b375c6
Danilo <danilodevsp@gmail.com>
2025-10-02 22:19:22 -0300

    fix: fix bug export

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,15 +1,18 @@
 import { TouchableOpacity, Text } from "react-native";
 import styles from "./styles";
 
-export function Button({ variant = "primary", onPress }) {
+export function Button({ variant = "primary", onPress, currency }) {
     return (
         <TouchableOpacity
             onPress={onPress}
             style={[
                 styles.button,
                 variant === "primary" ? styles.buttonPrimary : styles.buttonSecondary
-            ]}>
-            <Text style={styles.buttonText}>Clique</Text>
-        </TouchableOpacity >
-    )
+            ]}
+        >
+            <Text style={styles.buttonText}>
+                {currency.code}
+            </Text>
+        </TouchableOpacity>
+    );
 }
